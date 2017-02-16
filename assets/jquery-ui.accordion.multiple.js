5a61bd746ee23770ce0c504906342b7e12080617
Artyom 'Sleepwalker' Fedosov <mail@asleepwalker.ru>
2017-02-16 21:53:10 +0200

    Add UMD

--- assets/jquery-ui.accordion.multiple.js
+++ assets/jquery-ui.accordion.multiple.js
@@ -1,4 +1,28 @@
-(function($) {
+/*
+*	jQueryUI.Accordion.Multiple, v1.0.1
+*	(c) 2014–2017 Artyom "Sleepwalker" Fedosov <mail@asleepwalker.ru>
+*	https://github.com/asleepwalker/jquery-ui.tabs.neighbors.js
+*/
+
+(function(factory) {
+	if (typeof define === 'function' && define.amd) {
+		define(['jquery'], factory);
+	} else if (typeof module === 'object' && module.exports) {
+		module.exports = function(root, jQuery) {
+			if (jQuery === undefined) {
+				if (typeof window !== 'undefined') {
+					jQuery = require('jquery');
+				} else {
+					jQuery = require('jquery')(root);
+				}
+			}
+			factory(jQuery);
+			return jQuery;
+		};
+	} else {
+		factory(jQuery);
+	}
+}(function($) {
 
 	var originalToggle = $.ui.accordion.prototype._toggle;
 
@@ -17,4 +41,4 @@
 		}
 	});
 
-})(jQuery);
+}));
